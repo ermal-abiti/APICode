28449326c8a3af425a2bb8bd3912378530c98c4a
matrixleo <73724759+matrixleo@users.noreply.github.com>
2021-05-30 14:59:59 +0200

    Update BlogPost.cs

--- Domain/BlogPost.cs
+++ Domain/BlogPost.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace Domain
 {
     public class BlogPost
-    {
-        
+    {        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DatePosted { get; set; }
+
+        public User User { get; set;}
+
     }
 }
