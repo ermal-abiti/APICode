ae3c3737031ed22aa7589677dea68ffb3049bb6b
Ermal <ermal.abiti@gmail.com>
2021-06-01 00:37:46 +0200

    Migration for Post Comment

---
+++ Persistence/Migrations/20210531223624_PostComment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class PostComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PostComment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    content = table.Column<string>(type: "TEXT", nullable: true),
+                    DatePosted = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    BlogPostId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostComment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostComment_BlogPosts_BlogPostId",
+                        column: x => x.BlogPostId,
+                        principalTable: "BlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostComment_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostComment_BlogPostId",
+                table: "PostComment",
+                column: "BlogPostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostComment_UserId",
+                table: "PostComment",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostComment");
+        }
+    }
+}
