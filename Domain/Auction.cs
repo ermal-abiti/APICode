d257ba7f4190707672bff076488f58fd9b88f6ef
Iljas Neziri <iljasneziri7@gmail.com>
2021-06-02 21:44:21 +0200

    Filet C# per Ankand

---
+++ Domain/Auction.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Domain{
+    public class Auciton{
+
+    }   
+}
